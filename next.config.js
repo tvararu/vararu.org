20a968267f42046a6d0f85ac9c579764224a75c6
Theodor Vararu <theo@vararu.org>
2019-06-02 11:01:18 +0100

    Remove webpack-visualizer-plugin

--- next.config.js
+++ next.config.js
@@ -5,12 +5,5 @@ module.exports = {
     '/cv/': {page: '/cv/'},
     '/pug/': {page: '/pug/'},
     '/the-last-question/': {page: '/the-last-question/'}
-  }),
-  webpack: (config, {dev}) => {
-    if (dev) {
-      const Visualizer = require('webpack-visualizer-plugin')
-      config.plugins.push(new Visualizer())
-    }
-    return config
-  }
+  })
 }
