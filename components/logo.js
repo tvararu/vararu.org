3f49bf6511a09a54d26d9ab9724ad1b01e39a63c
Theodor Vararu <theo@vararu.org>
2020-03-22 13:09:22 +0000

    Reimplement gyro logo using a single animation
    Accelerometer permisions now require user consent on iOS, which is a bit too involved for a gimmick.

--- components/logo.js
+++ components/logo.js
@@ -1,141 +1,55 @@
 import { Component } from "react";
-import throttle from "raf-throttle";
-import { Motion, spring } from "react-motion";
 import SvgLogo from "./svg-logo";
 
-const limitTo01Range = (x, a) => x / (Math.abs(x) + a);
+const Logo = () => (
+  <div className="wrapper" role="presentation">
+    <style jsx>{`
+      .wrapper {
+        animation: mount 1.5s;
+        height: 100%;
+        transform-style: preserve-3d;
+        transform: rotate3d(-0.4, -1, 0, 25deg);
+        width: 100%;
+      }
 
-export default class Logo extends Component {
-  constructor(props) {
-    super(props);
+      .background,
+      .foreground {
+        height: 100%;
+        position: absolute;
+        width: 100%;
+      }
 
-    this.state = {
-      tiltx: 0,
-      tilty: 0,
-      degree: 0
-    };
+      .background {
+        transform: scale(0.99);
+      }
 
-    this.handleMouseMove = throttle(this.handleMouseMove.bind(this));
-    this.handleMouseLeave = this.handleMouseLeave.bind(this);
-    this.handleDeviceOrientation = throttle(
-      this.handleDeviceOrientation.bind(this)
-    );
-    this.tilt = this.tilt.bind(this);
-  }
+      .foreground {
+        color: #fff;
+        transform: translateZ(10px);
+      }
 
-  componentDidMount() {
-    document.addEventListener("mousemove", this.handleMouseMove);
-    document.addEventListener("mouseleave", this.handleMouseLeave);
-    window.addEventListener("deviceorientation", this.handleDeviceOrientation);
-  }
+      @keyframes mount {
+        0% {
+          opacity: 0;
+          transform: rotate3d(0, 0, 0, 0deg);
+        }
 
-  componentWillUnmount() {
-    document.removeEventListener("mousemove", this.handleMouseMove);
-    document.removeEventListener("mouseleave", this.handleMouseLeave);
-    window.removeEventListener(
-      "deviceorientation",
-      this.handleDeviceOrientation
-    );
-  }
+        25% {
+          opacity: 1;
+        }
 
-  handleMouseMove(event) {
-    const rect = this.$wrapper.getBoundingClientRect();
-    const cx = Math.ceil(rect.width / 2.0);
-    const cy = Math.ceil(rect.height / 2.0);
-    const dx = limitTo01Range(event.pageX - cx - rect.left, 128) * 128;
-    const dy = limitTo01Range(event.pageY - cy - rect.top, 64) * 64;
+        35% {
+          transform: rotate3d(-0.4, -1, 0, 35deg);
+        }
+      }
+    `}</style>
+    <div className="background">
+      <SvgLogo />
+    </div>
+    <div className="foreground">
+      <SvgLogo />
+    </div>
+  </div>
+);
 
-    const tiltx = -(dy / cy);
-    const tilty = dx / cx;
-    this.tilt(tiltx, tilty);
-  }
-
-  handleMouseLeave() {
-    this.tilt(0, 0);
-  }
-
-  handleDeviceOrientation(event) {
-    const tiltx = limitTo01Range(event.beta, 10) * -1;
-    const tilty = limitTo01Range(event.gamma, 10);
-    this.tilt(tiltx, tilty);
-  }
-
-  tilt(tiltx, tilty) {
-    const radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2));
-    const degree = radius * 20;
-    this.setState({ tiltx, tilty, degree });
-  }
-
-  render() {
-    const { tiltx, tilty, degree } = this.state;
-    const springParams = { stiffness: 100, damping: 8 };
-
-    return (
-      <Motion
-        defaultStyle={{
-          backgroundTransformScale: 1,
-          degree: 0,
-          tiltx: 0,
-          tilty: 0,
-          foregroundTranslateZ: 0
-        }}
-        style={{
-          backgroundTransformScale: spring(0.999),
-          degree: spring(degree, springParams),
-          tiltx: spring(tiltx, springParams),
-          tilty: spring(tilty, springParams),
-          foregroundTranslateZ: spring(10)
-        }}
-        children={style => (
-          <div
-            className="wrapper"
-            ref={wrapper => {
-              this.$wrapper = wrapper;
-            }}
-            role="presentation"
-            style={{
-              transform: `rotate3d(${style.tiltx}, ${style.tilty}, 0, ${
-                style.degree
-              }deg)`
-            }}
-          >
-            <style jsx>{`
-              .wrapper {
-                height: 100%;
-                transform-style: preserve-3d;
-                width: 100%;
-              }
-
-              .background,
-              .foreground {
-                height: 100%;
-                position: absolute;
-                width: 100%;
-              }
-
-              .foreground {
-                color: #fff;
-              }
-            `}</style>
-            <div
-              className="background"
-              style={{
-                transform: `scale(${style.backgroundTransformScale})`
-              }}
-            >
-              <SvgLogo />
-            </div>
-            <div
-              className="foreground"
-              style={{
-                transform: `translateZ(${style.foregroundTranslateZ}px)`
-              }}
-            >
-              <SvgLogo />
-            </div>
-          </div>
-        )}
-      />
-    );
-  }
-}
+export default Logo;
