dd0964b8de78e4f43b740b1d142c62f488748f01
Theodor Vararu <theo@vararu.org>
2025-11-09 10:23:19 +0700

    Replace vercel.json with next.config.ts
    This is the more modern way to handle redirects.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/commas/:trailing?",
+        destination: "https://tvararu.github.io/commas/",
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: "/pug/:trailing?",
+        destination: "https://tvararu.github.io/pug/",
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: "/the-last-question/:trailing?",
+        destination: "https://tvararu.github.io/the-last-question/",
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
