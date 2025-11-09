118c00b0e4f1b1e7217ea372a8fc91491313f4eb
Theodor Vararu <theo@vararu.org>
2025-11-09 13:25:26 +0700

    Make heading mailto link
    This is more straightforward than the vararu.org link that does nothing.

--- components/header.tsx
+++ components/header.tsx
@@ -5,20 +5,14 @@ import { ThemeToggle } from "@/components/theme-toggle";
 
 export default function Header() {
   return (
-    <header className="my-8 flex h-16 items-center justify-between sm:h-20">
+    <header className="my-10 flex h-16 items-center justify-between sm:h-20">
       <div className="flex flex-col gap-4">
         <h1 className="font-sans text-xl font-bold sm:text-2xl">
-          theo@
-          <Link href="/" className="underline">
-            vararu.org
+          <Link href="mailto:theo@vararu.org" className="underline">
+            theo@vararu.org
           </Link>
         </h1>
         <div className="flex gap-4">
-          <SocialIcon
-            href="mailto:theo@vararu.org"
-            icon="/envelope.svg"
-            label="Send me an email"
-          />
           <SocialIcon
             href="https://github.com/tvararu"
             icon="/github.svg"
