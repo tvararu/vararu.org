e0a449c25e2265c2189d5f267b403d1f1131f257
Theodor Vararu <theo@vararu.org>
2025-11-10 11:29:49 +0700

    Add hover animation to logo
    Makes it more juicy and interactive.

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,10 +1,44 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Logo() {
+  const [rotation, setRotation] = useState({ x: 0, y: 0 });
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseEnter = () =>
+    requestAnimationFrame(() => setIsHovering(true));
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * -10;
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    setRotation({ x: rotateX, y: rotateY });
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+    setRotation({ x: 0, y: 0 });
+  };
+
   return (
     <div
-      className="h-16 w-32 transform-[rotate3d(-0.4,-1,0,25deg)]
-        perspective-normal transform-3d"
+      className={`h-16 w-32 perspective-normal transform-3d ${
+        !isHovering ? "transition-transform duration-150 ease-in-out" : ""
+      }`}
+      style={{
+        transform: `rotate3d(-0.4, -1, 0, 25deg) rotateX(${rotation.x}deg) rotateY(${rotation.y}deg)`,
+      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
       role="presentation"
     >
       <Image
